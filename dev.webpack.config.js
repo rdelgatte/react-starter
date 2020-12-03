68de5074d9706790c07b69e1bd5f928d37ff8356
Remi Delgatte <remi.delgatte@decathlon.com>
2020-12-03 16:06:40 +0100

    Configure public path for webpack

--- dev.webpack.config.js
+++ dev.webpack.config.js
@@ -1,13 +1,12 @@
-const merge = require("webpack-merge");
-const path = require("path");
-const common = require("./common.webpack.config.js");
+const merge = require('webpack-merge');
+const common = require('./common.webpack.config.js');
 
 module.exports = merge(common, {
-  mode: "development",
-  devtool: "inline-source-map",
+  mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    open: "Google Chrome",
-    port: 9000,
-  },
+    open: 'Google Chrome',
+    publicPath: '/',
+    port: 9000
+  }
 });
