cf98f338990ac4a90ffd06dc11adb35332512dbf
Remi Delgatte <remi.delgatte@decathlon.com>
2020-12-03 17:16:07 +0100

    cleanup

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,5 @@
-import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
+import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
+import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
